604bb19ff30b51173cf1eaf8baabc29cc36bca7d
hx <2459267179@qq.com>
2025-10-21 16:56:43 +0800

    项目创建（已经配置所用到的基本中间件）

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+
+const app = createApp(App)
+
+//pinia
+app.use(createPinia())
+//  router
+app.use(router)
+// element plus
+app.use(ElementPlus)
+
+app.mount('#app')
