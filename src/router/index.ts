b48bc07fc2c0097c7c0fe6e62fec6ec2a52db5e6
wsy <16335973+wsy-wonderfully-shaping-youths@user.noreply.gitee.com>
2025-11-20 21:29:45 +0800

    仿照学习添加了我的路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import {systemRouters} from "@/router/system/SystemRouters.ts";
 import {userInfoRouters} from "@/router/auth/AuthRouters.ts";
 import {teacherRouters} from "@/router/teacher/TeacherSideRouter.ts";
+import {contactUsRouter} from "@/router/contactUs/ContactUsRouter.ts";
 
 // 页面路由合并
 const routes = [
     ...systemRouters,
     ...userInfoRouters,
     ...teacherRouters,
+    ...contactUsRouter,
 ]
 
 const router = createRouter({
