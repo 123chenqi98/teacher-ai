4c151c1b6716920642392970efd6f1ae8bb03bd2
wsy <16335973+wsy-wonderfully-shaping-youths@user.noreply.gitee.com>
2025-12-10 18:36:58 +0800

    重新添加视图

---
+++ src/views/settings/SettingViews.vue
@@ -0,0 +1,22 @@
+<!-- 导包处-->
+<script setup lang="ts">
+import Setting from "@/components/settings/home/Settings.vue";
+</script>
+
+
+
+<!--组件要显示的主要东西放置板块-->
+<template>
+  <div class="Contact-Us-Page-Box">
+
+  <Setting/>
+
+  </div>
+</template>
+
+
+
+<!--存放Css样式的区域-->
+<style scoped>
+
+</style>
